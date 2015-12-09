cf09e1166294d978453b8dcfdae876df9eb3dedb
Marcus Schäfer <ms@suse.com>
2015-12-09 14:29:38 +0100

    Run dracut as background process for vmx images
    The dracut process takes quite some time and blocks the boot
    process. In cloud frameworks instance boot time matters and
    with this patch the dracut process runs in parallel with the
    rest of the boot process. This fixes (bnc#971621)

--- modules/KIWILinuxRC.sh
+++ modules/KIWILinuxRC.sh
@@ -1607,12 +1607,22 @@ function setupInitrd {
         #--------------------------------------
         if [ -x "$dracutExec" ]; then
             # 1. dracut
-            Echo "Creating dracut based initrd"
             params=" -f /boot/initrd-$kernel_version $kernel_version"
-            if ! $dracutExec -H $params;then
-                Echo "Can't create initrd with dracut"
-                systemIntegrity=unknown
-                bootLoaderOK=0
+            if [[ $kiwi_initrdname =~ vmxboot ]];then
+                Echo "Creating dracut based initrd (background process)"
+                # run dracut in the background to speed up the boot.
+                # We loose the status check of the call and reboot is
+                # only safe after the call has finished. Therefore this
+                # is only done for simple vmx type images which are
+                # mostly used in cloud frameworks
+                $dracutExec -H $params &>/dev/null </dev/null &
+            else
+                Echo "Creating dracut based initrd"
+                if ! $dracutExec -H $params;then
+                    Echo "Can't create initrd with dracut"
+                    systemIntegrity=unknown
+                    bootLoaderOK=0
+                fi
             fi
         elif [ -x "$mkinitrdExec" ]; then
             # 2. mkinitrd
@@ -11494,6 +11504,7 @@ function initialize {
         if [ ! -z "$kiwi_bootloader" ];then
             export loader=$kiwi_bootloader
         fi
+        export kiwi_initrdname=$kiwi_iname
     fi
     #======================================
     # Check partitioner capabilities
